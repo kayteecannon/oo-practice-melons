b9be34a0e78100b03330f05414dca152c98db02f
Kaytee Cannon <kaytee.cannon@me.com>
2020-10-08 20:47:59 +0000

    Implements print_pairing_info method

--- harvest.py
+++ harvest.py
@@ -51,7 +51,11 @@ def make_melon_types():
 def print_pairing_info(melon_types):
     """Prints information about each melon type's pairings."""
 
-    # Fill in the rest
+    for melon in melon_types:
+        print(f'{melon.name} pairs with:')
+        for pairing in melon.pairings:
+            print(f'- {pairing}')
+        print('\n')
 
 def make_melon_type_lookup(melon_types):
     """Takes a list of MelonTypes and returns a dictionary of melon type by code."""
